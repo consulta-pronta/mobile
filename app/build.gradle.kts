4a4fe8a11cebeb37af6db6aa0c75a6c901212a37
calimuun <calielian@proton.me>
2026-08-09 21:14:50 -0300

    add profile picture custom view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
 	implementation(libs.material)
 	implementation(libs.androidx.activity)
 	implementation(libs.androidx.constraintlayout)
+	implementation(libs.bumptech.glide)
+
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
