4c2738f2c8afcc0de5777665e83235d596424c02
calimuun <calielian@proton.me>
2026-08-20 10:58:52 -0300

    add password validation

--- app/src/main/java/com/unnebulous/consultapronta/Cadastro.kt
+++ app/src/main/java/com/unnebulous/consultapronta/Cadastro.kt
@@ -1,11 +1,14 @@
 package com.unnebulous.consultapronta
 
+import android.app.ActivityManager
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.TextViewCompat
 import androidx.core.widget.doOnTextChanged
 import com.unnebulous.consultapronta.databinding.FragmentCadastroBinding
 
@@ -60,8 +63,42 @@ class Cadastro : Fragment() {
 			}
 		}
 		
-		binding.passwordInput.doOnTextChanged { text, start, before, count ->
+		binding.passwordInput.doOnTextChanged { text, _, _, _ ->
+			text?.let {
+				val drawables = Array(3) {0}
+				val colors = Array(3) {0}
 
+				var haveLowercase = false
+				var haveUppercase = false
+				var haveNumber = false
+
+				it.forEach { char ->
+					haveLowercase = (char.isLowerCase() || haveLowercase)
+					haveUppercase = (char.isUpperCase() || haveUppercase)
+					haveNumber = (char.isDigit() || haveNumber)
+				}
+
+				val isGreaterOrEqualThan8 = it.length > 8
+				val haveLowerAndUppercase = haveLowercase && haveUppercase
+
+				colors[0] = if (isGreaterOrEqualThan8) R.color.success else R.color.error
+				drawables[0] = if (isGreaterOrEqualThan8) R.drawable.ic_check_circle else R.drawable.ic_error_circle
+
+				colors[1] = if (haveLowerAndUppercase) R.color.success else R.color.error
+				drawables[1] = if (haveLowerAndUppercase) R.drawable.ic_check_circle else R.drawable.ic_error_circle
+
+				colors[2] = if (haveNumber) R.color.success else R.color.error
+				drawables[2] = if (haveNumber) R.drawable.ic_check_circle else R.drawable.ic_error_circle
+
+				binding.passwordErrorLength.setCompoundDrawablesRelativeWithIntrinsicBounds(drawables[0], 0, 0, 0)
+				binding.passwordErrorLength.setTextColor(colors[0])
+
+				binding.passwordErrorCaps.setCompoundDrawablesRelativeWithIntrinsicBounds(drawables[1], 0, 0, 0)
+				binding.passwordErrorCaps.setTextColor(colors[1])
+
+				binding.passwordErrorNumbers.setCompoundDrawablesRelativeWithIntrinsicBounds(drawables[2], 0, 0, 0)
+				binding.passwordErrorNumbers.setTextColor(colors[2])
+			}
 		}
 	}
 
