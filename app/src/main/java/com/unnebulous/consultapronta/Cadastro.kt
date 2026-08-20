89fbc62e4d19657e3aa1a08cbe1296b953667540
leafcabal <rafaelcabral4100@gmail.com>
2026-08-20 10:49:55 -0300

    auth: connect signup to firebase auth
    Works on: #41

--- app/src/main/java/com/unnebulous/consultapronta/Cadastro.kt
+++ app/src/main/java/com/unnebulous/consultapronta/Cadastro.kt
@@ -2,16 +2,29 @@ package com.unnebulous.consultapronta
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.tasks.Task
+import com.google.firebase.Firebase
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.unnebulous.consultapronta.databinding.FragmentCadastroBinding
 
 class Cadastro : Fragment() {
 
 	private var _binding: FragmentCadastroBinding? = null
 	private val binding get() = _binding!!
+	private lateinit var auth: FirebaseAuth
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		auth = Firebase.auth
+	}
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -47,6 +60,11 @@ class Cadastro : Fragment() {
 		}
 
 		binding.createAccountButton.setOnClickListener {
+			val email = binding.emailInput.text.toString()
+			val password = binding.passwordInput.text.toString()
+
+			auth.createUserWithEmailAndPassword(email, password)
+				.addOnCompleteListener { task -> handlePostSignUp(task) }
 			startActivity(Intent(requireActivity(), MainActivity::class.java))
 			requireActivity().finish()
 		}
@@ -55,5 +73,18 @@ class Cadastro : Fragment() {
 	override fun onDestroyView() {
 		super.onDestroyView()
 		_binding = null
+	}
+
+
+	private fun handlePostSignUp(task: Task<AuthResult>) {
+		if (task.isSuccessful) {
+			Log.i("auth", "signUpWithEmail:success")
+
+			val activity = requireActivity()
+			startActivity(Intent(activity, MainActivity::class.java))
+			activity.finish()
+		} else {
+			Log.w("auth", "signUpWithEmail:failure", task.exception)
+		}
 	}
 }
