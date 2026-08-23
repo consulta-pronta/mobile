aa8ec85b93a7e70a5780fd0ee04e272395854937
Orionsz642 <gabiaeva0216@gmail.com>
2026-08-23 20:26:30 -0300

    add home-screen

--- app/src/main/java/com/unnebulous/consultapronta/Home.kt
+++ app/src/main/java/com/unnebulous/consultapronta/Home.kt
@@ -9,9 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-import com.unnebulous.consultapronta.views.Symptom
-import com.unnebulous.consultapronta.views.SymptomAdapter
-
 class Home : Fragment() {
 
 	override fun onCreateView(
@@ -26,20 +23,6 @@ class Home : Fragment() {
 			false
 		)
 
-		val recyclerView =
-			view.findViewById<RecyclerView>(
-				R.id.recents_recycler
-			)
 
-		recyclerView.layoutManager =
-			LinearLayoutManager(requireContext())
-
-		val symptoms = mutableListOf<Symptom>()
-
-		val adapter = SymptomAdapter(symptoms)
-
-		recyclerView.adapter = adapter
-
-		return view
-	}
-}
+		return TODO("Provide the return value")
+	}}
